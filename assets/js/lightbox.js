4967c85a556c62ca59bcc41e419761600e7311cd
Matt Ayre <mattayre@Matts-MacBook-Pro.local>
2016-03-13 21:52:04 +0000

    commit8

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -2,7 +2,7 @@ function CreateImage(filename,caption){
     var line1="";
     var line1=line1+'<div class="col-md-6">';
     var line1=line1+'<div class="project-image">';
-    var line1=line1+'<img src="assets/img/'+filename+'" onclick="OpenLightbox(\'assets/img/'+filename+'\',\''+caption+'\'); return false" />';
+    var line1=line1+'<img src="assets/img/imgsm/'+filename+'" onclick="OpenLightbox(\'assets/img/imglg/'+filename+'\',\''+caption+'\'); return false" />';
     var line1=line1+'</div>';
     var line1=line1+'</div>';
     document.write(line1);
@@ -11,7 +11,7 @@ function CreateImage(filename,caption){
 function OpenLightbox(filename,caption) {
     document.getElementById('lightboximage').src="";
     document.getElementById('lightboximage').src=filename;
-    document.getElementById('lighboxcaption').innerHTML=caption+"<br><br>Click anywhere to close";
+    document.getElementById('lighboxcaption').innerHTML="<i class='pe-7s-close-circle' style = 'postion:relative;left:20px;font-size:30px;'></i>"
 } 
 
 function LoadLightbox() {
